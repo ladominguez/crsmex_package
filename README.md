6c41afe6bf67c7d4ace2b25f9771f0f02db051b7
Luis (Suanshuan) <ladominguez12@gmail.com>
2026-09-07 20:47:12 -0600

    Modernize package installation

--- README.md
+++ README.md
@@ -1,7 +1,81 @@
-# crsmex_package
-Installation
+# CRSMEX
 
+CRSMEX is a Python package for characteristic repeating earthquake analysis.
+
+## Installation
+
+### 1. Clone the repository
+
+```bash
+git clone https://github.com/ladominguez/crsmex_package.git
+cd crsmex_package
+```
+
+### 2. Create and activate a Python environment
+
+For example, using Conda:
+
+```bash
+conda create -n crsmex python=3.12
+conda activate crsmex
+```
+
+### 3. Install CRSMEX
+
+For development, install the package in editable mode:
+
+```bash
+python -m pip install -e .
+```
+
+The editable installation means that changes made to the source code are immediately available without reinstalling the package.
+
+For a regular installation, use:
+
+```bash
+python -m pip install .
+```
+
+The package automatically installs its required dependencies:
+
+* NumPy
+* SciPy
+* Matplotlib
+* ObsPy
+* Pandas
+
+### 4. Verify the installation
+
+Run:
+
+```bash
+python -c "import crsmex; print(crsmex.__file__)"
+```
+
+The output should point to the `crsmex` directory inside the cloned repository.
+
+## Development
+
+If you are modifying CRSMEX, use the editable installation:
+
+```bash
+python -m pip install -e .
+```
+
+After modifying the source code, there is no need to reinstall the package.
+
+## Requirements
+
+The file `requirements.txt` contains the versions of the packages used for the development environment and can be used when reproducing that environment.
+
+## Notes
+
+The package should be installed with `pip`. The older command:
+
+```bash
 python setup.py install
+```
 
-Add the following enviromental variable
-export PYTHONPATH=$PYTHONPATH:/path_to_the_package
+is deprecated and should not be used.
+
+It is also no longer necessary to manually add the CRSMEX directory to `PYTHONPATH` when the package is installed with pip.
--- setup.py
+++ setup.py
@@ -1,37 +1,33 @@
 from setuptools import setup, find_packages
-import os
 
-lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = f"{lib_folder}/requirements.txt"
-install_requires = []
-if os.path.isfile(requirement_path):
-    with open(requirement_path) as f:
-        install_requires = f.read().splitlines()
+VERSION = "0.1.1"
+DESCRIPTION = "CRSMEX Python package"
+LONG_DESCRIPTION = "Characteristic repeating earthquake package."
 
-VERSION = '0.1.1' 
-DESCRIPTION = 'CRSMEX Python package'
-LONG_DESCRIPTION = 'Characteristic repeating earthquake package.'
-
-# Setting up
 setup(
-       # the name must match the folder name 'verysimplemodule'
-        name="crsmex_package", 
-        version=VERSION,
-        author="Luis A. Dominguez",
-        author_email="<ladominguez12@gmail.com>",
-        description=DESCRIPTION,
-        long_description=LONG_DESCRIPTION,
-        packages=find_packages(),
-        install_requires=install_requires, # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
-        
-        keywords=['python', 'earthquake'],
-        classifiers= [
-            "Development Status :: 3 - Alpha",
-            "Intended Audience :: Education",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 3",
-            "Operating System :: MacOS :: MacOS X",
-            "Operating System :: Microsoft :: Windows",
-        ]
+    name="crsmex",
+    version=VERSION,
+    author="Luis A. Dominguez",
+    author_email="ladominguez12@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[
+        "numpy>=1.26",
+        "scipy>=1.11",
+        "matplotlib>=3.8",
+        "obspy>=1.4",
+        "pandas>=2.2",
+    ],
+    python_requires=">=3.9",
+    keywords=[
+        "python",
+        "earthquake",
+        "seismology",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+    ],
 )
